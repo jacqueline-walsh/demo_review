c491f51952b7f39f14171a290232354ecd5c8bb3
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2018-02-04 09:09:37 +0000

    link reviews with restaurants

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,8 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:edit, :update, :destroy]
+  before_action :set_restaurant
   before_action :authenticate_user!
-  # GET /reviews
-  # GET /reviews.json
- 
+
   # GET /reviews/new
   def new
     @review = Review.new
@@ -18,10 +17,11 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
-
+    @review.restaurant_id = @restaurant.id
+    
     respond_to do |format|
       if @review.save
-        format.html { redirect_to root_path, notice: 'Review was successfully created.' }
+        format.html { redirect_to @restaurant, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
@@ -60,6 +60,10 @@ class ReviewsController < ApplicationController
       @review = Review.find(params[:id])
     end
 
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:restaurant_id])
+    end
+    
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
       params.require(:review).permit(:rating, :comment)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 	
-  resources :reviews, except: [:show, :index]
   devise_for :users
-  resources :restaurants
-
+  resources :restaurants do
+  	resources :reviews, except: [:show, :index]
+	end
+	
   root 'restaurants#index'
 
   get 'pages/about'
