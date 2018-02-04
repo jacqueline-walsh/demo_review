f3d0126f2dc3d5602df7ed2b38330f1ac347fa7c
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2018-02-04 13:18:37 +0000

    add data validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,9 @@ class Restaurant < ActiveRecord::Base
   
   mount_uploader :image, ImageUploader
   has_many :reviews
+  validates :name, :address, :phone, :website, :image, presence: true
+  validates :phone, format: { with: /\A\(\d{3}\) \d{3} \d{3}\z/,
+    message: "must be in the format of: (091) 123 123" }
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+    message: "must start with http:// or https//" }        
 end
